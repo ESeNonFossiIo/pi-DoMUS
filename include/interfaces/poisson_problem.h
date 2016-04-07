ed66cb4bcccb35ae5fcc374e4df1f86c41fbe0ff
ESeNonFossiIo <esenonfossiio@gmail.com>
2016-04-07 18:37:18 +0200

    bug

--- include/interfaces/poisson_problem.h
+++ include/interfaces/poisson_problem.h
@@ -136,7 +136,7 @@ PoissonProblem<dim,spacedim,LAC>::compute_system_operators(const std::vector<sha
   // PETSc_preconditioner.reset  (new PETScWrappers::PreconditionJacobi());
   // PETSc_preconditioner->initialize(matrices[0]->block(0,0));
   // 
-  // auto A  = linear_operator<LAPETSc::VectorType::BlockType>( matrices[0]->block(0,0) );
+  auto A  = linear_operator<LAPETSc::VectorType::BlockType>( matrices[0]->block(0,0) );
   // 
   // LinearOperator<LAPETSc::VectorType::BlockType> P_inv;
   // 
@@ -145,14 +145,14 @@ PoissonProblem<dim,spacedim,LAC>::compute_system_operators(const std::vector<sha
   // auto P00 = P_inv;
   // 
   // // ASSEMBLE THE PROBLEM:
-  // system_op  = block_operator<1, 1, LAPETSc::VectorType>({{
-  //     {{ A }}
-  //   }
-  // });
-  // 
-  // prec_op = block_operator<1, 1, LAPETSc::VectorType>({{
-  //     {{ P00}} ,
-  //   }
-  // });
+  system_op  = block_operator<1, 1, LAPETSc::VectorType>({{
+      {{ A }}
+    }
+  });
+  
+  prec_op = block_operator<1, 1, LAPETSc::VectorType>({{
+      {{ A }} ,
+    }
+  });
 }
 #endif
